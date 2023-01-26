91e8b9465b30b212db41dc12f86872a86d4fed9d
samnbogen <119772308+samnbogen@users.noreply.github.com>
2023-01-26 15:02:06 -0700

    Add files via upload

---
+++ PersonLab1/PersonLab1/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLab1
+{
+    internal class Person
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string favoriteColor;
+        private bool isWorking;
+
+        public int Id { get => id; set => id = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+        public string FavoriteColor { get => favoriteColor; set => favoriteColor = value; }
+        public bool IsWorking { get => isWorking; set => isWorking = value; }
+
+        public Person(int id, string firstName, string lastName, int age, string favoriteColor, bool isWorking)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            Age = age;
+            FavoriteColor = favoriteColor;
+            IsWorking = isWorking;
+        }
+
+        public override string ToString()
+        {
+            string formatted = "";
+
+            formatted += "PersonId: " + id + "\n" + "First name: " + firstName + "\n" + "Last name: " + lastName + "\n" + "Favorite color: " + favoriteColor + "\n" + "Age: " + age + "\n" + "Is Working: " + isWorking + "\n";
+
+            return formatted;
+        }
+
+        public virtual void DisplayPersonInfo()
+        {
+            Console.WriteLine(id + ": " + firstName + " " + lastName + " favorite color is " + favoriteColor + "\n");
+        }
+
+        public virtual void ChangeFavoriteColour()
+        {
+            favoriteColor = "white";
+        }
+
+        public virtual void GetAgeInTenYears()
+        {
+            age += 10;
+            Console.WriteLine(firstName + " " + lastName + "’s Age in 10 years is: " + age + "\n");
+            age -= 10;
+        }
+
+    }
+}
---
+++ PersonLab1/PersonLab1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Ian", "Brooks", 30, "red", true);
+            Person person2 = new Person(2, "Gina", "James", 18, "green", false);
+            Person person3 = new Person(3, "Mike", "Briscoe", 45, "blue", true);
+            Person person4 = new Person (4, "Mary", "Beals", 28, "yellow", true);
+
+            person2.DisplayPersonInfo();
+
+            Console.WriteLine(person3.ToString());
+
+            person2.ChangeFavoriteColour();
+
+            person2.DisplayPersonInfo();
+
+            person4.GetAgeInTenYears();
+
+            List<double> agelist = new List<double>() { person1.Age, person2.Age, person3.Age, person4.Age };
+
+            List<Person> personlist = new List<Person>() { person1, person2, person3, person4 };    
+
+            double ageavrg = agelist.Sum() / 4;
+
+            Console.WriteLine("Average age is: " + ageavrg + "\n");
+
+            var nameresult = from x in personlist
+                         where x.FirstName.Substring(0, 1) == "M"
+                         select x;
+
+            foreach (var person in nameresult)
+                Console.WriteLine(person.ToString());
+
+            var colorresult = from x in personlist
+                             where x.FavoriteColor == "blue"
+                             select x;
+
+            foreach (var person in colorresult)
+                Console.WriteLine(person.ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
